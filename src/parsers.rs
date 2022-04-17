e587a9509b6e0660565c22252357421d4b74cf95
Daniel Duan <daniel@duan.ca>
2022-04-17 16:20:44 -0700

    Improve line handling

--- src/parsers.rs
+++ src/parsers.rs
@@ -5,41 +5,46 @@ use std::option::Option;
 
 pub fn ripgrep(input: &[u8]) -> (Vec<u8>, Vec<Location>) {
     lazy_static! {
-        static ref RE_PATH: Regex = Regex::new(r#"^\x1B\[0m\x1B\[\d+?m(.+?)\x1B\[0m$"#).unwrap();
+        static ref RE_PATH: Regex = Regex::new(r#"^\x1b\[0m\x1b\[\d+?m(.+?)\x1b\[0m$"#).unwrap();
+        static ref RE_PATH_: Regex = Regex::new(r#"^\x1b\[0m\x1b\[\d+?m(.+?)\x1b\[0m$"#).unwrap();
         static ref RE_LINE: Regex =
-            Regex::new(r#"^\x1B\[0m\x1B\[\d+?m(\d+?)\x1B\[0m:.+?"#).unwrap();
+            Regex::new(r#"^\x1b\[0m\x1b\[\d+?m(\d+?)\x1b\[0m:.+?"#).unwrap();
     }
 
-    let mut output: Vec<String> = vec![];
+    let mut output = String::new();
+    _ = std::fs::write("/tmp/debug", input);
     let input_str = std::str::from_utf8(input).unwrap();
 
     let mut locations: Vec<Location> = Vec::new();
     let mut file: Option<&str> = None;
-    for line in input_str.split('\n') {
+    for line in input_str.lines() {
         if let Some(line_match) = RE_PATH.captures(line) {
             file = Some(line_match.get(1).unwrap().as_str());
         } else if let Some(line_match) = RE_LINE.captures(line) {
             if let Ok(line_number) = line_match.get(1).unwrap().as_str().parse::<u64>() {
                 if let Some(current_file) = file {
-                    locations.push(Location {
+                    let new_location = Location {
                         path: current_file.to_string(),
                         line: Some(line_number),
                         column: None,
-                    })
+                    };
+
+                    locations.push(new_location);
                 }
 
-                output.push(format!(
-                    "[\x1B[0m\x1B[31m{}\x1B[0m] {}",
+                output = format!(
+                    "{}[\x1b[0m\x1b[31m{}\x1b[0m] {}\n",
+                    output,
                     locations.len(),
                     line
-                ));
+                );
             }
             continue;
         }
-        output.push(line.to_string());
+        output = format!("{}{}\n", output, line);
     }
 
-    let output_data: Vec<u8> = output.join("\n").as_bytes().to_owned();
+    let output_data: Vec<u8> = output.as_bytes().to_owned();
     _ = std::fs::write("/tmp/test_output", &output_data);
     (output_data, locations)
 }
