19f80ac067494ef59517b7300600eeed9bd08bca
Daniel Duan <daniel@duan.ca>
2022-04-25 23:06:29 -0700

    Re-arrange completion script generation targets (#27)

--- build.rs
+++ build.rs
@@ -1,18 +1,18 @@
 use clap::CommandFactory;
 use clap_complete::{generate_to, Shell};
 use std::fs;
-use std::path::PathBuf;
+use std::env;
 use Shell::*;
 
 include!("src/interface.rs");
 
 fn main() {
-    let outdir: PathBuf = [env!("CARGO_MANIFEST_DIR"), "scripts", "completion"]
-        .iter()
-        .collect();
+    let outdir = env::var("SHELL_COMPLETIONS_DIR").or_else(|_| env::var("OUT_DIR")).unwrap();
     fs::create_dir_all(&outdir).unwrap();
     let mut cmd = Interface::command();
     for shell in [Bash, PowerShell, Fish, Elvish, Zsh] {
         generate_to(shell, &mut cmd, "ea", &outdir).unwrap();
     }
+
+    println!("cargo:rerun-if-changed=src/interface.rs");
 }
