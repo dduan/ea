eda0ba75b35c857818ca94d1f158526206ab9090
Daniel Duan <daniel@duan.ca>
2022-04-26 00:19:02 -0700

    Add docstring/help text for interface (#28)

--- scripts/completion/_ea.ps1
+++ scripts/completion/_ea.ps1
@@ -25,14 +25,14 @@ Register-ArgumentCompleter -Native -CommandName 'ea' -ScriptBlock {
             [CompletionResult]::new('--help', 'help', [CompletionResultType]::ParameterName, 'Print help information')
             [CompletionResult]::new('-V', 'V', [CompletionResultType]::ParameterName, 'Print version information')
             [CompletionResult]::new('--version', 'version', [CompletionResultType]::ParameterName, 'Print version information')
-            [CompletionResult]::new('run', 'run', [CompletionResultType]::ParameterValue, 'run')
-            [CompletionResult]::new('list', 'list', [CompletionResultType]::ParameterValue, 'list')
-            [CompletionResult]::new('print', 'print', [CompletionResultType]::ParameterValue, 'print')
+            [CompletionResult]::new('run', 'run', [CompletionResultType]::ParameterValue, 'Run EXECUTABLE through `ea`. Expecting its output to be the format of STYLE. Arguments for EXECUTABLE must come after `--`. For example, `rg Vec src` becomes:')
+            [CompletionResult]::new('list', 'list', [CompletionResultType]::ParameterValue, 'List locations found from the latest `ea run` output. This is the default subcommand. Running `ea` is the same as running `ea list`')
+            [CompletionResult]::new('print', 'print', [CompletionResultType]::ParameterValue, 'Print the location info associated with NUMBER. Optionally, customize the output FORMAT. Also availble as the shorthand `ea p ...`')
             [CompletionResult]::new('help', 'help', [CompletionResultType]::ParameterValue, 'Print this message or the help of the given subcommand(s)')
             break
         }
         'ea;run' {
-            [CompletionResult]::new('--debug', 'debug', [CompletionResultType]::ParameterName, 'debug')
+            [CompletionResult]::new('--debug', 'debug', [CompletionResultType]::ParameterName, 'Write debug info at <debug_files_base_name.*>')
             [CompletionResult]::new('-h', 'h', [CompletionResultType]::ParameterName, 'Print help information')
             [CompletionResult]::new('--help', 'help', [CompletionResultType]::ParameterName, 'Print help information')
             break
--- scripts/completion/ea.fish
+++ scripts/completion/ea.fish
@@ -1,10 +1,10 @@
 complete -c ea -n "__fish_use_subcommand" -s h -l help -d 'Print help information'
 complete -c ea -n "__fish_use_subcommand" -s V -l version -d 'Print version information'
-complete -c ea -n "__fish_use_subcommand" -f -a "run"
-complete -c ea -n "__fish_use_subcommand" -f -a "list"
-complete -c ea -n "__fish_use_subcommand" -f -a "print"
+complete -c ea -n "__fish_use_subcommand" -f -a "run" -d 'Run EXECUTABLE through `ea`. Expecting its output to be the format of STYLE. Arguments for EXECUTABLE must come after `--`. For example, `rg Vec src` becomes:'
+complete -c ea -n "__fish_use_subcommand" -f -a "list" -d 'List locations found from the latest `ea run` output. This is the default subcommand. Running `ea` is the same as running `ea list`'
+complete -c ea -n "__fish_use_subcommand" -f -a "print" -d 'Print the location info associated with NUMBER. Optionally, customize the output FORMAT. Also availble as the shorthand `ea p ...`'
 complete -c ea -n "__fish_use_subcommand" -f -a "help" -d 'Print this message or the help of the given subcommand(s)'
-complete -c ea -n "__fish_seen_subcommand_from run" -l debug -r -F
+complete -c ea -n "__fish_seen_subcommand_from run" -l debug -d 'Write debug info at <debug_files_base_name.*>' -r -F
 complete -c ea -n "__fish_seen_subcommand_from run" -s h -l help -d 'Print help information'
 complete -c ea -n "__fish_seen_subcommand_from list" -s h -l help -d 'Print help information'
 complete -c ea -n "__fish_seen_subcommand_from print" -s h -l help -d 'Print help information'
