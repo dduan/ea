4cf2205c4842746e3c0c14709ae7c4783d326b35
Daniel Duan <daniel@duan.ca>
2022-04-30 14:40:53 -0700

    Default column/line number for `ea print` to 1 (#42)
    Closes #40

--- src/commands/print.rs
+++ src/commands/print.rs
@@ -4,10 +4,10 @@ pub fn print(number: usize, format: &str) {
     let mut result = format.to_string();
     let list = archive::read();
     if number <= list.len() {
-        let location = &list[number-1];
+        let location = &list[number - 1];
         result = result.replace("{path}", &location.path);
-        result = result.replace("{line}", &location.line.unwrap_or(0).to_string());
-        result = result.replace("{column}", &location.column.unwrap_or(0).to_string());
+        result = result.replace("{line}", &location.line.unwrap_or(1).to_string());
+        result = result.replace("{column}", &location.column.unwrap_or(1).to_string());
 
         println!("{}", &result);
     }
