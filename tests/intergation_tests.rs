fca32500a7620e0ccd3c4280e5524d98b288b476
Daniel Duan <daniel@duan.ca>
2022-05-23 10:05:10 -0700

    Improve inter-process error signaling
    When error occurs, add the following at the end of child processes output:
    
    1. exit status code (1 byte)
    2. internal error code (1 byte)
    3. error siginal (4 bytes, `0xdeadbeef`)
    
    Add integration tests for child process failed execution and non-success child process exits.

--- tests/intergation_tests.rs
+++ tests/intergation_tests.rs
@@ -1,6 +1,6 @@
 use assert_cmd::prelude::CommandCargoExt;
 use std::path::PathBuf;
-use std::process::Command;
+use std::process;
 use std::io::Read;
 use pty::fork::Fork;
 
@@ -11,8 +11,8 @@ fn find_subsequence(haystack: &[u8], needle: &[u8]) -> Option<usize> {
 #[test]
 // Run `ea run` in a terminal with a sample app. Check output is reasonably modified by `ea`.
 fn invoke_subprocess_via_pty() -> Result<(), Box<dyn std::error::Error>> {
-    let mut ea = Command::cargo_bin("ea")?;
-    let printer_cmd = Command::cargo_bin("printfile")?;
+    let mut ea = process::Command::cargo_bin("ea")?;
+    let printer_cmd = process::Command::cargo_bin("printfile")?;
     let printer = printer_cmd.get_program();
     let input_path: PathBuf = [
         env!("CARGO_MANIFEST_DIR"),
@@ -26,7 +26,6 @@ fn invoke_subprocess_via_pty() -> Result<(), Box<dyn std::error::Error>> {
     let mut output = Vec::new();
     if let Ok(mut parent) = fork.is_parent() {
         _ = parent.read_to_end(&mut output);
-        assert!(find_subsequence(&output, b"[[0m[31m1[0m] [0m[32m20[0m:").is_some());
     } else {
         ea
             .args(["run", "grouped"])
@@ -35,6 +34,44 @@ fn invoke_subprocess_via_pty() -> Result<(), Box<dyn std::error::Error>> {
             .arg(input_path)
             .status()
             .expect("execution fails");
+        process::exit(0)
     }
+    assert!(find_subsequence(&output, b"[[0m[31m1[0m] [0m[32m20[0m:").is_some());
+    Ok(())
+}
+
+#[test]
+fn subprocess_command_error() -> Result<(), Box<dyn std::error::Error>> {
+    let mut ea = process::Command::cargo_bin("ea")?;
+    let fork = Fork::from_ptmx().unwrap();
+    let mut output = Vec::new();
+    if let Ok(mut parent) = fork.is_parent() {
+        _ = parent.read_to_end(&mut output);
+    } else {
+        ea
+            .args(["run", "rust", "cargo", "--", "clippppy"])
+            .status()
+            .expect("execution fails");
+        process::exit(0)
+    }
+    assert!(find_subsequence(&output, b"[ea]: cargo encountered an error").is_some());
+    Ok(())
+}
+
+#[test]
+fn subprocess_execution_fail() -> Result<(), Box<dyn std::error::Error>> {
+    let mut ea = process::Command::cargo_bin("ea")?;
+    let fork = Fork::from_ptmx().unwrap();
+    let mut output = Vec::new();
+    if let Ok(mut parent) = fork.is_parent() {
+        _ = parent.read_to_end(&mut output);
+    } else {
+        ea
+            .args(["run", "rust", "/lmao/i/do/not/exist"])
+            .status()
+            .expect("execution fails");
+        process::exit(0)
+    }
+    assert!(find_subsequence(&output, b"[ea]: could not execute /lmao/i/do/not/exist").is_some());
     Ok(())
 }
