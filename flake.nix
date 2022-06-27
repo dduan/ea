ec1736fc0cd580ba7ed53ae956be8bf1c1a095a2
Daniel Duan <daniel@duan.ca>
2022-06-27 00:37:25 -0700

    fixup! Bump version to 0.2.1

--- flake.nix
+++ flake.nix
@@ -18,7 +18,7 @@
             pname = info.name;
             version = info.version;
             src = ./.;
-            cargoSha256 = "sha256-UOZcFTqH3nym+v2W92gkY5UXJZb1SkWgNAm83JWHSt8=";
+            cargoSha256 = "sha256-1x9LQ1MkdNZLga+pAahkwaI6tlbdT41nwB+D1XVLBwY=";
             lockFile = ./Cargo.lock;
             nativeBuildInputs = [ installShellFiles pandoc ];
             preFixup = ''
